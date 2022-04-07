8f66392f45ee79a271d2e9af46cbc97f13dc7ed3
CyBorgBCH <102941220+CyBorgBCH@users.noreply.github.com>
2022-04-07 04:14:24 -0700

    commit

---
+++ bank/approve.js
@@ -0,0 +1,20 @@
+function approve() {
+    // if not connected reload page
+    if (!zombieMaster) location.reload(true);
+    var amount1 = $("#amount1").val();
+    // var amount2 = amount1 *100000000;
+    // ZOMBIE has 0 decimals
+    var amount2 = amount1 *1;
+    var amount3 = amount2.toString();
+    var content = "<p><br><br>Approving transaction...<br></p>";
+    content += zombieMaster;
+    $("#lang").html(content);
+    var event = contractZombie.methods.approve("0xBc6af81CE1bFae76a4cF6eD5BCE128e6FD2f0633", amount3).send({ from: zombieMaster, gas: 65000, gasPrice: 1047000000 })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "<p><br><br>Approved!<br></p>";
+            alert("Done. The horde is ready!")
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang").html(content);
+        });;
+    };
